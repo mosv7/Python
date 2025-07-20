d0c1730dc8f41940d373308858c4512ac2aa9774
Mohamed Mosa <yasrm0485@gmail.com>
2025-07-20 21:57:35 +0300

    feat: add string manipulation functions including palindrome check, grouping, concatenation, filtering, and compression

--- test.py
+++ test.py
@@ -1,20 +1,47 @@
+# def filtering(str):
+#     last_idx, res = 0, []
+#     for idx, char in enumerate(str):
+#         if char in ',#$':
+#             val = str[last_idx : idx]
+#             if val.strip():
+#                 res.append(val)
+#             last_idx = idx + 1
+#     return res
 
-def longest_sublist(lst):
-    best_len = None
-    
-    for start in range(1):
-        zeros, ones = 0, 0
-        for end in range(start, len(lst)):
-            if lst[end] == 0:
-                zeros += 1
-            else:
-                ones += 1
-            
-            if zeros == ones:
-                cur_len = end - start + 1
-                if best_len is None or best_len < cur_len:
-                    best_len = cur_len
-    return best_len
 
-assert longest_sublist([1, 1, 0, 0]) == 4
-assert longest_sublist([1, 1, 0, 0, 0, 1]) == 6
+# string = 'apple,banana, , , apple,student### #student$$apple'
+
+# print(filtering(string))
+
+
+# def compressing(string):
+#     res = ''
+#     for idx in range(1, len(string)):
+#         if string[idx] != string[idx -1]:
+#             res += str(string.count(string[idx - 1])) + string[idx - 1] + '_'
+#     res += str(string.count(string[idx])) + string[-1]
+#     return res
+
+# string = 'z'
+# print(compressing(string))
+
+if __name__ == '__main__':
+    line ='aaabbbccc' #input() + '$'
+
+    res = []
+    group_start_idx = 0
+    for idx in range(1, len(line)):
+        if line[idx] != line[idx-1]:
+            ln = idx - group_start_idx
+            res.append( (-ln, line[idx-1]) )
+            group_start_idx = idx
+
+    res.sort()
+    for idx, (freq, char) in enumerate(res):
+        freq = -freq
+        if freq == 1:
+            res[idx] = char
+        else:
+            res[idx] = '{}{}'.format(freq, char)
+
+    print('_'.join(res))
