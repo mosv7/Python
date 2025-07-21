fee8de324a3f329d3cf509d9d69ef3c1442ed0d9
Mohamed Mosa <yasrm0485@gmail.com>
2025-07-21 21:09:13 +0300

    feat: update string manipulation functions and add palindrome check

--- test.py
+++ test.py
@@ -25,23 +25,82 @@
 # string = 'z'
 # print(compressing(string))
 
-if __name__ == '__main__':
-    line ='aaabbbccc' #input() + '$'
+# if __name__ == '__main__':
+#     line ='aaabbbccc' #input() + '$'
 
-    res = []
-    group_start_idx = 0
-    for idx in range(1, len(line)):
-        if line[idx] != line[idx-1]:
-            ln = idx - group_start_idx
-            res.append( (-ln, line[idx-1]) )
-            group_start_idx = idx
+#     res = []
+#     group_start_idx = 0
+#     for idx in range(1, len(line)):
+#         if line[idx] != line[idx-1]:
+#             ln = idx - group_start_idx
+#             res.append( (-ln, line[idx-1]) )
+#             group_start_idx = idx
 
-    res.sort()
-    for idx, (freq, char) in enumerate(res):
-        freq = -freq
-        if freq == 1:
-            res[idx] = char
-        else:
-            res[idx] = '{}{}'.format(freq, char)
+#     res.sort()
+#     for idx, (freq, char) in enumerate(res):
+#         freq = -freq
+#         if freq == 1:
+#             res[idx] = char
+#         else:
+#             res[idx] = '{}{}'.format(freq, char)
 
-    print('_'.join(res))
+#     print('_'.join(res))
+
+# def our_replace(main_str, pattern, repalce_with):
+#     idx = 0
+#     new_str = ''
+#     n = len(pattern)
+
+#     while idx < len(main_str):
+#         substr = main_str[idx:idx + n]
+
+#         if substr == pattern:
+#             new_str += repalce_with
+#             idx += n
+#         else:
+#             new_str += main_str[idx]
+#             idx += 1
+
+#     return new_str 
+
+
+# print(our_replace('aabcabaaad', 'aa', 'x'))
+
+# def containsDuplicate(nums: list[int]) -> bool:
+#     freq = [0] * (max(nums) + 1)
+    
+#     for value in nums:
+#         freq[value] += 1
+    
+#     for i in range(len(freq)):
+#         if freq[i] > 1:
+#             return True
+#     return False
+
+# print(containsDuplicate([1,2,3,2]))
+
+
+# def isPalindrome(s: str) -> bool:
+#         s.replace(',', '').replace(':', '').replace(' ', '').lower()
+#         return s == s[::-1]
+
+# print(isPalindrome("A man, a plan, a canal: Panama"))
+
+def isPalindrome(s: str) -> bool:
+    left, right = 0, len(s) - 1
+    
+    while left < right:
+        while left < right and not s[left].isalnum():
+            left += 1
+        while left < right and not s[right].isalnum():
+            right -= 1
+        
+        if s[left].lower() != s[right].lower():
+            return False
+        
+        left += 1
+        right -= 1
+    
+    return True
+
+print(isPalindrome("A man, a plan, a canal: Panama"))
