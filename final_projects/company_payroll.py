7a1b8da3118629e8c2fd647f581309d392a32e6f
mosv7 <yasrm0485@gmail.com>
2025-09-10 01:53:20 +0300

    feat: implement invoice validation system with mandatory and complete validators

--- final_projects/company_payroll.py
+++ final_projects/company_payroll.py
@@ -1,3 +1,5 @@
+from abc import ABC, abstractmethod
+
 class StaffMember:
     def __init__(self, name, address):
         self.name = name
@@ -66,17 +68,64 @@ class Food(Item):
         super().__init__(desc, price_per_one, quantity)
         self.expiration_date = expiration_date
 
+class ValidationRule(ABC):
+    @abstractmethod
+    def is_valid_rule(self, invoice):
+        pass
+
+class TaxesValidationRule(ValidationRule):
+    def is_valid_rule(self, invoice):
+        print('validating taxesvalidationrule')
+        return True
+
+class SuppliersDealsValidationRule(ValidationRule):
+    def is_valid_rule(self, invoice):
+        print('validating suppliersdealsvalidationrule')
+        return True
+
+class InvoiceValidator:
+    def __init__(self, rules):
+        self.rules = rules
+    
+    def validate(self, invoice: ValidationRule):
+        if not self.rules:
+            raise ValueError("No validation rules provided")
+        
+        for rule in self.rules:
+            if not rule.is_valid_rule(invoice):
+                return False
+        return True
+
+class MandatoryInvoiceValidator(InvoiceValidator):
+    @staticmethod
+    def get_instance():
+        rules = [TaxesValidationRule()]
+        return MandatoryInvoiceValidator(rules)
+
+
+class CompleteInvoiceValidator(InvoiceValidator):
+    @staticmethod
+    def get_instance():
+        rules = [TaxesValidationRule(), SuppliersDealsValidationRule()]
+        return CompleteInvoiceValidator(rules)
+
+class ValidationError(BaseException):
+    pass
+
 class Invoice:
-    def __init__(self, invoice_id):
+    def __init__(self, invoice_id, validator: InvoiceValidator):
         self.invoice_id = invoice_id
+        self.validator = validator
         self.items = []
 
     def add_item(self, item):
         self.items.append(item)
-    
+
     @property
     def pay_amount(self):
-        return sum(item.price for item in self.items)
+        if not self.validator.validate(self):
+            raise ValidationError('One of the invoices are invalid')
+        return sum([item.price for item in self.items])
 
 class Payroll:
     def __init__(self):
@@ -93,21 +142,24 @@ class Company:
     def __init__(self):
         self.payroll = Payroll()
     
-    def run(self):
+    def fill_data(self, is_mandatory_validator=True):
         self.payroll.add_payable(Volunteer('Most', 'AddressV', 700))
         self.payroll.add_payable(HourlyEmployee('Belal', 'AddressH', 1, 10, 3))
-        self.payroll.add_payable(SalariedEmployee('Ziad', 'AddressS', 2, 3000))
-        self.payroll.add_payable(CommissionSalariedEmployee('Safa', 'AddressC', 6, 2500, 0.001, 5000))
+        self.payroll.add_payable(SalariedEmployee('Ziad', 'AddressS1', 2, 3000))
+        self.payroll.add_payable(SalariedEmployee('Ashraf', 'AddressS1', 2, 3000))
+        self.payroll.add_payable(CommissionSalariedEmployee('Safa', 'AddressC1', 6, 2500, 0.001, 5000))
+        self.payroll.add_payable(CommissionSalariedEmployee('ahmed', 'AddressC2', 6, 2500, 0.001, 5000))
 
-        invoice = Invoice(1234)
+        if is_mandatory_validator:
+            validator = MandatoryInvoiceValidator.get_instance()
+        else:
+            validator = CompleteInvoiceValidator.get_instance()
+        invoice = Invoice(1234, validator)
         invoice.add_item(Book('book1', 10, 7, ''))
         invoice.add_item(Food('food1', 5, 6, ''))
         self.payroll.add_payable(invoice)
 
-        print(self.payroll.pay_amount)
-
 if __name__ == "__main__":
     company = Company()
-    company.run()
-
-
+    company.fill_data(False)
+    print(f'Total Payroll: {company.payroll.pay_amount}')
