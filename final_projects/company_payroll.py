3031e1409a06aec9cd69e434a97d6e23f9d20ab5
mosv7 <yasrm0485@gmail.com>
2025-09-09 23:36:52 +0300

    feat: implement item management and payroll processing in Company class

--- final_projects/company_payroll.py
+++ final_projects/company_payroll.py
@@ -45,3 +45,69 @@ class Volunteer(StaffMember):
     def pay_amount(self):
         return self.current_payment
 
+class Item:
+    def __init__(self, desc, price_per_one, quantity):
+        self.desc = desc
+        self.price_per_one = price_per_one
+        self.quantity = quantity
+
+    @property
+    def price(self):
+        return self.price_per_one * self.quantity
+
+class Book(Item):
+    def __init__(self, desc, price_per_one, quantity, author):
+        super().__init__(desc, price_per_one, quantity)
+        self.author = author
+    
+
+class Food(Item):
+    def __init__(self, desc, price_per_one, quantity, expiration_date):
+        super().__init__(desc, price_per_one, quantity)
+        self.expiration_date = expiration_date
+
+class Invoice:
+    def __init__(self, invoice_id):
+        self.invoice_id = invoice_id
+        self.items = []
+
+    def add_item(self, item):
+        self.items.append(item)
+    
+    @property
+    def pay_amount(self):
+        return sum(item.price for item in self.items)
+
+class Payroll:
+    def __init__(self):
+        self.payabels = []
+    
+    def add_payable(self, payable):
+        self.payabels.append(payable)
+    
+    @property
+    def pay_amount(self):
+        return sum(payable.pay_amount for payable in self.payabels)
+
+class Company:
+    def __init__(self):
+        self.payroll = Payroll()
+    
+    def run(self):
+        self.payroll.add_payable(Volunteer('Most', 'AddressV', 700))
+        self.payroll.add_payable(HourlyEmployee('Belal', 'AddressH', 1, 10, 3))
+        self.payroll.add_payable(SalariedEmployee('Ziad', 'AddressS', 2, 3000))
+        self.payroll.add_payable(CommissionSalariedEmployee('Safa', 'AddressC', 6, 2500, 0.001, 5000))
+
+        invoice = Invoice(1234)
+        invoice.add_item(Book('book1', 10, 7, ''))
+        invoice.add_item(Food('food1', 5, 6, ''))
+        self.payroll.add_payable(invoice)
+
+        print(self.payroll.pay_amount)
+
+if __name__ == "__main__":
+    company = Company()
+    company.run()
+
+
